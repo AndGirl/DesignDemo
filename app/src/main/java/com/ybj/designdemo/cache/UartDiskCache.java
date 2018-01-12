47ddb56d192c4986d8cc35e4e21f5a3cb2aa43f9
YBJ <15390264297@163.com>
2018-01-12 19:01:14 +0800

    实现所有缓存方式

--- app/src/main/java/com/ybj/designdemo/cache/UartDiskCache.java
+++ app/src/main/java/com/ybj/designdemo/cache/UartDiskCache.java
@@ -1,14 +1,29 @@
 package com.ybj.designdemo.cache;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Environment;
+import android.util.Log;
+
+import com.ybj.designdemo.CloseUtils;
+import com.ybj.designdemo.ImageResizer;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by 杨阳洋 on 2018/1/9.
  * 磁盘缓存
  */
 
-public class UartDiskCache implements UartBitmapCache{
+public class UartDiskCache implements UartBitmapCache {
     /**
      * 1MB
      */
@@ -26,27 +41,54 @@ public class UartDiskCache implements UartBitmapCache{
      * Disk Cache Instance
      */
     private static UartDiskCache mUartDiskCache;
+    private ImageResizer mImageResizer = new ImageResizer();
 
-    public UartDiskCache(Context context){
+    public UartDiskCache(Context context) {
         initDiskCache(context);
     }
 
     /**
      * 初始化磁盘缓存
+     *
      * @param context
      */
     private void initDiskCache(Context context) {
+        try {
+            File cacheDir = getDiskCacheDir(context, IMAGE_DISK_CACHE);
+            if (!cacheDir.exists()) {
+                cacheDir.mkdirs();
+            }
+            mDiskLruCache = DiskLruCache.open(cacheDir, getAppVersion(context), 1, 50 * MB);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     /**
+     * @param context
+     * @return
+     */
+    private int getAppVersion(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(),
+                    0);
+            return info.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    /**
      * 获取磁盘缓存
+     *
      * @param context
      */
     public static UartDiskCache getDiskCache(Context context) {
-        if(mUartDiskCache == null) {
-            synchronized (UartDiskCache.class){
-                if(mUartDiskCache == null) {
+        if (mUartDiskCache == null) {
+            synchronized (UartDiskCache.class) {
+                if (mUartDiskCache == null) {
                     mUartDiskCache = new UartDiskCache(context);
                 }
             }
@@ -54,33 +96,134 @@ public class UartDiskCache implements UartBitmapCache{
         return mUartDiskCache;
     }
 
-    @Override
-    public Bitmap get(String key) {
-        return null;
+    /**
+     * 获取缓存路径
+     *
+     * @param context
+     * @param uniqueName
+     * @return
+     */
+    public File getDiskCacheDir(Context context, String uniqueName) {
+        String cachePath;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Log.d("", "### context : " + context + ", dir = " + context.getExternalCacheDir());
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return new File(cachePath + File.separator + uniqueName);
     }
 
     @Override
-    public void set(String key, Bitmap bitmap) {
+    public Bitmap get(String key) {
+        InputStream fileInputStream = null;
+        try {
+            fileInputStream = getInputStream(key);
+            FileInputStream inputStream = (FileInputStream) fileInputStream;
+            FileDescriptor fd = inputStream.getFD();
+            Bitmap bitmap = mImageResizer.decodeSampledBitmapFromFileDescriptor(fd, 500, 500);
+            if (bitmap != null) {
+                return bitmap;
+            }
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            CloseUtils.closeQuietly(fileInputStream);
+        }
+        return null;
+    }
+
+    /**
+     * MD5加密处理
+     *
+     * @param md5
+     * @return
+     */
+    private InputStream getInputStream(String md5) {
+        DiskLruCache.Snapshot snapshot;
+        try {
+            snapshot = mDiskLruCache.get(md5);
+            if (snapshot != null) {
+                return snapshot.getInputStream(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 存储图片
+     *
+     * @param key
+     * @param bitmap
+     */
+    @Override
+    public void set(String key, Bitmap bitmap) {
+        DiskLruCache.Editor edit = null;
+        try {
+            edit = mDiskLruCache.edit(key);
+            if(edit != null) {
+                OutputStream outputStream = edit.newOutputStream(0);
+                if(writeBitmapToDisk(bitmap,outputStream)) {
+                    //写入磁盘
+                    edit.commit();
+                }else{
+                    edit.abort();
+                }
+                IOUtil.closeQuietly(outputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 写入磁盘缓存
+     *
+     * @param bitmap
+     * @param outputStream
+     * @return
+     */
+    private boolean writeBitmapToDisk(Bitmap bitmap, OutputStream outputStream) {
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream, 8 * 1024);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bufferedOutputStream);
+        boolean result = true;
+        try {
+            bufferedOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        return 0;
+        return (int) mDiskLruCache.size();
     }
 
     @Override
     public int maxSize() {
-        return 0;
+        return (int) mDiskLruCache.getMaxSize();
     }
 
     @Override
     public void clear() {
-
+        try {
+            mDiskLruCache.delete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void clearKeyUri(String keyPrefix) {
-
+        try {
+            mDiskLruCache.remove(keyPrefix);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/ybj/designdemo/cache/UartDoubleCache.java
+++ app/src/main/java/com/ybj/designdemo/cache/UartDoubleCache.java
@@ -1,9 +1,71 @@
 package com.ybj.designdemo.cache;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+
 /**
  * Created by 杨阳洋 on 2018/1/9.
  * 磁盘缓存和内存缓存
  */
 
-public class UartDoubleCache {
+public class UartDoubleCache implements UartBitmapCache {
+
+    /**
+     * 创建两种缓存的类
+     */
+    UartDiskCache mUartDiskCache;
+    UartMemoryCache mUartMemoryCache = new UartMemoryCache();
+
+    public UartDoubleCache(Context context) {
+        mUartDiskCache = UartDiskCache.getDiskCache(context);
+    }
+
+    /**
+     * 读取的话先从内存中读取，在读磁盘
+     * @param key
+     * @return
+     */
+    @Override
+    public Bitmap get(String key) {
+        Bitmap bitmap = mUartMemoryCache.get(key);
+        if(bitmap == null) {
+            bitmap = mUartDiskCache.get(key);
+            saveBitmapIntoMemory(key,bitmap);
+        }
+        return bitmap;
+    }
+
+    private void saveBitmapIntoMemory(String key,Bitmap bitmap){
+        if(bitmap != null) {
+            mUartMemoryCache.set(key,bitmap);
+        }
+    }
+
+    @Override
+    public void set(String key, Bitmap bitmap) {
+        mUartDiskCache.set(key,bitmap);
+        mUartMemoryCache.set(key,bitmap);
+    }
+
+    @Override
+    public int size() {
+        return mUartDiskCache.size() + mUartMemoryCache.size();
+    }
+
+    @Override
+    public int maxSize() {
+        return mUartDiskCache.maxSize() + mUartMemoryCache.maxSize();
+    }
+
+    @Override
+    public void clear() {
+        mUartDiskCache.clear();
+        mUartMemoryCache.clear();
+    }
+
+    @Override
+    public void clearKeyUri(String keyPrefix) {
+        mUartDiskCache.clearKeyUri(keyPrefix);
+        mUartMemoryCache.clearKeyUri(keyPrefix);
+    }
 }
